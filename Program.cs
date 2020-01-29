99bdfa6c46bd1c3b141329c6e8b404a73e55c35d
alex1feu17 <alexyd@hotmail.ca>
2020-01-28 20:25:50 -0500

    exercise2.2 Complete

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace exercise2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom = "ALEXY DUVAL";
+            Console.WriteLine(nom);
+        }
+    }
+}
